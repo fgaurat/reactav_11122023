d5c5a86c2833aa32c6c1433798c0e8bc0357640a
Frédéric GAURAT <fgaurat@gmail.com>
2023-12-11 17:00:00 +0100

    add HOC

--- todos-app/src/pages/Home/Home.tsx
+++ todos-app/src/pages/Home/Home.tsx
@@ -6,29 +6,35 @@ import { TodoDAO } from "../../core/TodoDAO";
 import useFetchTodos from "../../hooks/useFetchTodos";
 
 function Home() {
-  const {todos,setTodos,isLoading} = useFetchTodos()
+  const { todos, setTodos, isLoading } = useFetchTodos();
 
-  const CompletedTodoList = HOCCompletedTodoList(TodoList)
+  const CompletedTodoList = HOCCompletedTodoList(TodoList);
 
+  const doDelete = async (todo: Todo) => {
+    const dao = new TodoDAO();
+    await dao.delete(todo);
+    const filteredTodos = todos.filter((t) => t.id !== todo.id);
+    setTodos(filteredTodos);
+  };
 
-  const doDelete = async (todo:Todo)=>{
-    const dao = new TodoDAO()
-    await dao.delete(todo)
-    const filteredTodos = todos.filter( t => t.id!== todo.id)
-    setTodos(filteredTodos)
-  }
-const submitTodo = async (todo:Todo)=>{
-    const dao = new TodoDAO()
-    const newTodo = await dao.save(todo)
-    setTodos([...todos,newTodo])
-}
+  const submitTodo = async (todo: Todo) => {
+    const dao = new TodoDAO();
+    const newTodo = await dao.save(todo);
+    setTodos([...todos, newTodo]);
+  };
+
+  
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Home</h1>
-      <TodoForm submitTodo={submitTodo}/>
+      <TodoForm submitTodo={submitTodo} />
       <hr />
       {/* <TodoList todos={todos} isLoading={isLoading} doDelete={doDelete}/> */}
-      <CompletedTodoList todos={todos} isLoading={isLoading} doDelete={doDelete}/>
+      <CompletedTodoList
+        todos={todos}
+        isLoading={isLoading}
+        doDelete={doDelete}
+      />
     </div>
   );
 }
