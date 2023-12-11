b88469a2d6c6dfd3610e4432a805157715f709a5
Frédéric GAURAT <fgaurat@gmail.com>
2023-12-11 15:53:05 +0100

    fix TodoForm

--- todos-app/src/components/TodoForm/TodoForm.tsx
+++ todos-app/src/components/TodoForm/TodoForm.tsx
@@ -1,19 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface ITodoForm {
+  title: string;
+  completed: boolean;
+}
 
 function TodoForm() {
-    const [theValue,setTheValue] = useState("")
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<ITodoForm>();
+
+  const onSubmit: SubmitHandler<ITodoForm> = (data) => console.log(data)
 
 
   return (
     <div>
-    <h2>TodoForm</h2>
+      <h2>TodoForm</h2>
 
-    <form action="">
-        <input type="text" value={theValue} onChange={e => setTheValue(e.target.value)}/>
-        {theValue}
-    </form>
+       <form className="space-y-6" method="POST" onSubmit={handleSubmit(onSubmit)}>
+            <div className="relative -space-y-px rounded-md shadow-sm">
+              <div className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300" />
+              <div>
+                <label htmlFor="" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  placeholder="Todo Title"
+                  {...register("title")}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  {...register("completed")}
+                />
+                <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
+                 Completed ?
+                </label>
+              </div>
+
+
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Ajouter
+              </button>
+            </div>
+          </form>
     </div>
-  )
+  );
 }
 
-export default TodoForm
+export default TodoForm;
